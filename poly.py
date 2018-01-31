6adeaeb80509b64c602c767c6642648c01e14267
travisthiesen <travisjthiesen@gmail.com>
2018-01-31 00:50:52 +0000

    Add documentation for poly_deff

--- poly.py
+++ poly.py
@@ -1,6 +1,12 @@
 def poly_diff(p):
+    "Computes the derivative of a polynomial"
     deg_p = len(p) - 1
+    # returns 0 if the inputed function is a constant
     if deg_p == 0:
         return [0]
+    # returns the derivative of a polynomial with exponents > 0
     else:
         return [k*p[k] for k in range(1,deg_p+1)]
+    
+    
+    
